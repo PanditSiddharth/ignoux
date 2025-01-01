846b6c704630d0f8cb7a577e0c4c36c96314fd6e
Siddharth sharma <mynmss03@gmail.com>
2025-01-01 19:53:46 +0530

    test

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import { Card } from "@/components/ui/card";
-
+import { useSession } from "next-auth/react";
 // eslint-disable-next-line
 const AdminPanelPage = () => {
+const { data: session } = useSession()
 
   return (
     <Card className="rounded-lg shadow-lg md:p-6 max-w-7xl min-w-32 w-full mx-auto">
-    hi
+    {JSON.stringify(session?.user || {})}
     </Card>
   );
 };
